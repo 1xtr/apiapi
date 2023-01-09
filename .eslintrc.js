ec7e32bcab7ea4c3560d85104eef15f20b6485ac
Aleksei Anisimov <i@trendco.dev>
2023-01-10 00:00:51 +0300

    Added eslint prettier

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    node: true,
+    es2022: true,
+  },
+  parser: '@typescript-eslint/parser',
+  extends: ['eslint:recommended', 'prettier'],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+        trailingComma: 'es5',
+      },
+    ],
+  },
+}
---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'es5',
+  tabWidth: 2,
+  endOfLine: 'lf',
+  semi: false,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+  bracketSameLine: false,
+}
